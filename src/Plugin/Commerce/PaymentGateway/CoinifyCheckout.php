acf1fdb0ae18da8d62309556252d8c7425058e1b
jdenasdetourris <jdetourris@liberal.co.nz>
2017-03-11 20:38:09 +0100

    changed code

--- src/Plugin/Commerce/PaymentGateway/CoinifyCheckout.php
+++ src/Plugin/Commerce/PaymentGateway/CoinifyCheckout.php
@@ -2,17 +2,20 @@
 
 namespace Drupal\commerce_coinify\Plugin\Commerce\PaymentGateway;
 
-use Drupal\commerce_payment\CreditCard;
+use Drupal\commerce_order\Entity\OrderInterface;
 use Drupal\commerce_payment\Entity\PaymentInterface;
-use Drupal\commerce_payment\Entity\PaymentMethodInterface;
-use Drupal\commerce_payment\Exception\HardDeclineException;
 use Drupal\commerce_payment\Exception\InvalidRequestException;
+use Drupal\commerce_payment\Exception\PaymentGatewayException;
 use Drupal\commerce_payment\PaymentMethodTypeManager;
 use Drupal\commerce_payment\PaymentTypeManager;
-use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OnsitePaymentGatewayBase;
+use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayBase;
 use Drupal\commerce_price\Price;
+use Drupal\commerce_price\RounderInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides the coinify Currency payment gateway.
@@ -30,16 +33,45 @@ use Drupal\Core\Form\FormStateInterface;
  *   },
  * )
  */
-class CoinifyCheckout extends OnsitePaymentGatewayBase implements CoinifyCheckoutInterface {
+class CoinifyCheckout extends OffsitePaymentGatewayBase implements CoinifyCheckoutInterface {
+
+ /**
+   * The HTTP client.
+   *
+   * @var \GuzzleHttp\Client
+   */
+  protected $httpClient;
+
+  /**
+   * The rounder.
+   *
+   * @var \Drupal\commerce_price\RounderInterface
+   */
+  protected $rounder;
 
   /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PaymentTypeManager $payment_type_manager, PaymentMethodTypeManager $payment_method_type_manager) {
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, PaymentTypeManager $payment_type_manager, PaymentMethodTypeManager $payment_method_type_manager, ClientInterface $client, RounderInterface $rounder) {
     parent::__construct($configuration, $plugin_id, $plugin_definition, $entity_type_manager, $payment_type_manager, $payment_method_type_manager);
+    $this->httpClient = $client;
+    $this->rounder = $rounder;
+  }
 
-    // You can create an instance of the SDK here and assign it to $this->api.
-    // Or inject Guzzle when there's no suitable SDK.
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('plugin.manager.commerce_payment_type'),
+      $container->get('plugin.manager.commerce_payment_method_type'),
+      $container->get('http_client'),
+      $container->get('commerce_price.rounder')
+    );
   }
 
   /**
@@ -47,8 +79,10 @@ class CoinifyCheckout extends OnsitePaymentGatewayBase implements CoinifyCheckou
    */
   public function defaultConfiguration() {
     return [
-      'api_key' => '',
-      'api_secret' => '',      
+      'api_username' => '',
+      'api_password' => '',
+      'signature' => '',
+      'solution_type' => 'Mark',
     ] + parent::defaultConfiguration();
   }
 
@@ -58,15 +92,12 @@ class CoinifyCheckout extends OnsitePaymentGatewayBase implements CoinifyCheckou
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
     $form = parent::buildConfigurationForm($form, $form_state);
 
-    // Example credential. Also needs matching schema in
-    // config/schema/$your_module.schema.yml.
     $form['api_key'] = [
       '#type' => 'textfield',
-      '#title' => $this->t('API key'),
+      '#title' => $this->t('API Key'),
       '#default_value' => $this->configuration['api_key'],
       '#required' => TRUE,
     ];
-
     $form['api_secret'] = [
       '#type' => 'textfield',
       '#title' => $this->t('API Secret'),
@@ -80,9 +111,32 @@ class CoinifyCheckout extends OnsitePaymentGatewayBase implements CoinifyCheckou
   /**
    * {@inheritdoc}
    */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
+   /* parent::validateConfigurationForm($form, $form_state);
+
+    if (!$form_state->getErrors()) {
+      $values = $form_state->getValue($form['#parents']);
+      if (!empty($values['api_key']) && !empty($values['api_secret'])) {
+        $this->configuration['api_key'] = $values['api_key'];
+        $this->configuration['api_secret'] = $values['api_secret'];
+
+        // Make a Coinify request to validate the Coinify API credentials.
+        $response = $this->doRequest([
+          'METHOD' => 'GetBalance',
+        ]);
+
+        if ($response['ACK'] != 'Success') {
+          $form_state->setError($form['api_key'], $this->t('Invalid API credentials.'));
+        }
+      }
+    }*/
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     parent::submitConfigurationForm($form, $form_state);
-
     if (!$form_state->getErrors()) {
       $values = $form_state->getValue($form['#parents']);
       $this->configuration['api_key'] = $values['api_key'];
@@ -93,158 +147,100 @@ class CoinifyCheckout extends OnsitePaymentGatewayBase implements CoinifyCheckou
   /**
    * {@inheritdoc}
    */
-  public function createPayment(PaymentInterface $payment, $capture = TRUE) {
-    if ($payment->getState()->value != 'new') {
-      throw new \InvalidArgumentException('The provided payment is in an invalid state.');
-    }
-    $payment_method = $payment->getPaymentMethod();
-    if (empty($payment_method)) {
-      throw new \InvalidArgumentException('The provided payment has no payment method referenced.');
-    }
-    if (REQUEST_TIME >= $payment_method->getExpiresTime()) {
-      throw new HardDeclineException('The provided payment method has expired');
-    }
-
-    // Add a built in test for testing decline exceptions.
-    /** @var \Drupal\address\Plugin\Field\FieldType\AddressItem $billing_address */
-    if ($billing_address = $payment_method->getBillingProfile()) {
-      $billing_address = $payment_method->getBillingProfile()->get('address')->first();
-      if ($billing_address->getPostalCode() == '53140') {
-        throw new HardDeclineException('The payment was declined');
-      }
-    }
-
-    // Perform the create payment request here, throw an exception if it fails.
-    // See \Drupal\commerce_payment\Exception for the available exceptions.
-    // Remember to take into account $capture when performing the request.
-    $amount = $payment->getAmount();
-    $payment_method_token = $payment_method->getRemoteId();
-    // The remote ID returned by the request.
-    $remote_id = '123456';
-
-    $payment->state = $capture ? 'capture_completed' : 'authorization';
-    $test = $this->getMode() == 'test';
-    $payment->setTest($test);
-    $payment->setRemoteId($remote_id);
-    $payment->setAuthorizedTime(REQUEST_TIME);
-    if ($capture) {
-      $payment->setCapturedTime(REQUEST_TIME);
-    }
-    $payment->save();
+  public function onReturn(OrderInterface $order, Request $request) {
+ 
   }
 
   /**
    * {@inheritdoc}
    */
   public function capturePayment(PaymentInterface $payment, Price $amount = NULL) {
-    if ($payment->getState()->value != 'authorization') {
-      throw new \InvalidArgumentException('Only payments in the "authorization" state can be captured.');
-    }
-    // If not specified, capture the entire amount.
-    $amount = $amount ?: $payment->getAmount();
-
-    // Perform the capture request here, throw an exception if it fails.
-    // See \Drupal\commerce_payment\Exception for the available exceptions.
-    $remote_id = $payment->getRemoteId();
-    $number = $amount->getNumber();
-
-    $payment->state = 'capture_completed';
-    $payment->setAmount($amount);
-    $payment->setCapturedTime(REQUEST_TIME);
-    $payment->save();
+ 
   }
 
   /**
    * {@inheritdoc}
    */
   public function voidPayment(PaymentInterface $payment) {
-    if ($payment->getState()->value != 'authorization') {
-      throw new \InvalidArgumentException('Only payments in the "authorization" state can be voided.');
-    }
 
-    // Perform the void request here, throw an exception if it fails.
-    // See \Drupal\commerce_payment\Exception for the available exceptions.
-    $remote_id = $payment->getRemoteId();
-
-    $payment->state = 'authorization_voided';
-    $payment->save();
   }
 
   /**
    * {@inheritdoc}
    */
   public function refundPayment(PaymentInterface $payment, Price $amount = NULL) {
-    if (!in_array($payment->getState()->value, ['capture_completed', 'capture_partially_refunded'])) {
-      throw new \InvalidArgumentException('Only payments in the "capture_completed" and "capture_partially_refunded" states can be refunded.');
-    }
-    // If not specified, refund the entire amount.
-    $amount = $amount ?: $payment->getAmount();
-    // Validate the requested amount.
-    $balance = $payment->getBalance();
-    if ($amount->greaterThan($balance)) {
-      throw new InvalidRequestException(sprintf("Can't refund more than %s.", $balance->__toString()));
-    }
 
-    // Perform the refund request here, throw an exception if it fails.
-    // See \Drupal\commerce_payment\Exception for the available exceptions.
-    $remote_id = $payment->getRemoteId();
-    $number = $amount->getNumber();
+  }
 
-    $old_refunded_amount = $payment->getRefundedAmount();
-    $new_refunded_amount = $old_refunded_amount->add($amount);
-    if ($new_refunded_amount->lessThan($payment->getAmount())) {
-      $payment->state = 'capture_partially_refunded';
+  /**
+   * {@inheritdoc}
+   */
+  public function getUrl() {
+    if ($this->getMode() == 'test') {
+      return 'https://api.sandbox.coinify.com/v3';
     }
     else {
-      $payment->state = 'capture_refunded';
+      return 'https://api.coinify.com/v3';
     }
-
-    $payment->setRefundedAmount($new_refunded_amount);
-    $payment->save();
   }
 
   /**
    * {@inheritdoc}
    */
-  public function createPaymentMethod(PaymentMethodInterface $payment_method, array $payment_details) {
-    $required_keys = [
-      // The expected keys are payment gateway specific and usually match
-      // the PaymentMethodAddForm form elements. They are expected to be valid.
-      'type', 'number', 'expiration',
+  public function setExpressCheckout(PaymentInterface $payment, array $extra) {
+    $order = $payment->getOrder();
+
+    $amount = $this->rounder->round($payment->getAmount());
+    $configuration = $this->getConfiguration();
+    //TODO Code the request.
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function doVoid(PaymentInterface $payment) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function doRefundTransaction(PaymentInterface $payment, array $extra) {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function doRequest(array $nvp_data) {
+    // Add the default name-value pairs to the array.
+    /*$configuration = $this->getConfiguration();
+    $nvp_data += [
+      // API credentials.
+      'USER' => $configuration['api_username'],
+      'PWD' => $configuration['api_password'],
+      'SIGNATURE' => $configuration['signature'],
+      'VERSION' => '124.0',
     ];
-    foreach ($required_keys as $required_key) {
-      if (empty($payment_details[$required_key])) {
-        throw new \InvalidArgumentException(sprintf('$payment_details must contain the %s key.', $required_key));
-      }
+
+    $mode = $this->getMode();
+    if ($mode == 'test') {
+      $url = 'https://api-3t.sandbox.paypal.com/nvp';
+    }
+    else {
+      $url = 'https://api-3t.paypal.com/nvp';
     }
 
-    // Perform the create request here, throw an exception if it fails.
-    // See \Drupal\commerce_payment\Exception for the available exceptions.
-    // You might need to do different API requests based on whether the
-    // payment method is reusable: $payment_method->isReusable().
-    // Non-reusable payment methods usually have an expiration timestamp.
-    $payment_method->card_type = $payment_details['type'];
-    // Only the last 4 numbers are safe to store.
-    $payment_method->card_number = substr($payment_details['number'], -4);
-    $payment_method->card_exp_month = $payment_details['expiration']['month'];
-    $payment_method->card_exp_year = $payment_details['expiration']['year'];
-    $expires = CreditCard::calculateExpirationTimestamp($payment_details['expiration']['month'], $payment_details['expiration']['year']);
-    // The remote ID returned by the request.
-    $remote_id = '789';
+    // Make PayPal request.
+    $request = $this->httpClient->post($url, [
+      'form_params' => $nvp_data,
+    ])->getBody()
+      ->getContents();
 
-    $payment_method->setRemoteId($remote_id);
-    $payment_method->setExpiresTime($expires);
-    $payment_method->save();
-  }
+    parse_str(html_entity_decode($request), $paypal_response);
 
-  /**
-   * {@inheritdoc}
-   */
-  public function deletePaymentMethod(PaymentMethodInterface $payment_method) {
-    // Delete the remote record here, throw an exception if it fails.
-    // See \Drupal\commerce_payment\Exception for the available exceptions.
-    // Delete the local entity.
-    $payment_method->delete();
+    return $paypal_response;*/
   }
 
 }
--- src/Plugin/Commerce/PaymentGateway/CoinifyCheckoutInterface.php
+++ src/Plugin/Commerce/PaymentGateway/CoinifyCheckoutInterface.php
@@ -2,13 +2,13 @@
 
 namespace Drupal\commerce_coinify\Plugin\Commerce\PaymentGateway;
 
-use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OnsitePaymentGatewayInterface;
+use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\OffsitePaymentGatewayInterface;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\SupportsAuthorizationsInterface;
 use Drupal\commerce_payment\Plugin\Commerce\PaymentGateway\SupportsRefundsInterface;
 
 /**
  * Provides the interface for the Coinify Checkout payment gateway.
  */
-interface CoinifyCheckoutInterface extends OnsitePaymentGatewayInterface, SupportsAuthorizationsInterface, SupportsRefundsInterface {
+interface CoinifyCheckoutInterface extends OffsitePaymentGatewayInterface, SupportsAuthorizationsInterface, SupportsRefundsInterface {
 
 }
